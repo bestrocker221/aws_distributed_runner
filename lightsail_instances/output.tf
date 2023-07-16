0dc675f8d1e40f8798a5cbf3ba22e1e562d6af8a
carloalberto.scola <albertoscola221@gmail.com>
2023-07-16 17:12:47 +0200

    deleted commented out sections

--- lightsail_instances/output.tf
+++ lightsail_instances/output.tf
@@ -1,21 +1,10 @@
 output "instance_ips" {
   value = [
     for i in range(var.instance_count) : {
-      instance_name = aws_lightsail_instance.lightsail_instance[i].name
-      username      = "ubuntu"
-      #public_ip = aws_lightsail_instance.lightsail_instance[i].public_ip_address
+      instance_name         = aws_lightsail_instance.lightsail_instance[i].name
+      username              = "ubuntu"
       static_ip             = aws_lightsail_static_ip_attachment.static_ip_assign[i].ip_address
       local_ssh_pubkey_path = var.local_ssh_pubkey_path
     }
   ]
 }
-
-/*
-output "lightsail-instance" {
-  value = [
-    aws_lightsail_instance.lightsail_instance.username,
-    #aws_lightsail_instance.lightsail_instance.public_ip_address,
-    aws_lightsail_static_ip_attachment.static_ip_assign.ip_address
-  ]
-}
-*/
